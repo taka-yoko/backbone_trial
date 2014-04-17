2de68bf63b3a1af2a7b2b8e624f364c310a49906
tamoriinu <tamoriinu@gmail.com>
2014-04-17 23:16:54 +0900

    v2

--- script/app.js
+++ script/app.js
@@ -1,9 +1,29 @@
-(function(){
-	var obj = new Backbone.Model();
+var Staff = Backbone.Model.extend({
+        defaults: {
+        	"name" : '',
+        	"age" : 0,
+        	"updateTime" : new Date() 	
+        },
+        initialize: function(){
+        	console.log("Staff[" + this.cid + "] : " + JSON.stringify(this));
+        }
+});
+
+var tmpStaff = new Staff();
+tmpStaff.set({name: "Murata", age: 15, id : 101});
+
+console.log("Staff[" + tmpStaff.cid + "]: " + JSON.stringify(tmpStaff));
+
+var tmpStaff2 = new Staff({name: "Kenichiro", age: 35, id: 102});
+
+var Staffs = Backbone.Collection.extend({
+	model: Staff	
+});
+
+var staffs = new Staffs([tmpStaff, tmpStaff2]);
+
+console.log("staffs: " + JSON.stringify(staffs));
+console.log("staffs.get(cid): " + JSON.stringify(staffs.get("c1")));
+console.log("staffs.at(index): " + JSON.stringify(staffs.at(1)));
+console.log("staffs.get(id): " + JSON.stringify(staffs.get(102)));
 	
-	obj.set({name: "Murata"});
-	obj.set({age: 20});
-	
-	console.log("obj: " + JSON.stringify(obj));
-	console.log("obj.name: " + obj.get("name"));
-}());
